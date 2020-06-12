9c646744323e43da2c798ad791d433ea76d70c7c
jfa-w <57237966+jfa-w@users.noreply.github.com>
2020-06-13 02:42:48 +0300

    Update sudfa.js

--- sudfa.js
+++ sudfa.js
@@ -1,5 +1,5 @@
  
-$(`<img style="margin-left: -1px;width:101%; margin-top: 0px;pointer-events:none;margin-bottom: 0px;" src="https://jfa-w.net/sico/1590854537522.gif " > <a id="jfa44"target="_blank"type="a"rel="nofollow"href="https://www.dookmobile.com"class="label-primary">Programming by Dook Mobile</a></div></div><center>
+$(`<img style="margin-left: -1px;width:101%; margin-top: 0px;pointer-events:none;margin-bottom: 0px;" src="https://jfa-w.net/sico/1590854537522.gif " > <a id="jfa44"target="_blank"type="a"rel="nofollow"href="https://www.dookmobile.com"class="label-primary">Dook Mobile</a></div></div><center>
 
  <a id="asim99" href="" class="fl mini  fa   fa fa-refresh btn btn-success"></a></div></div>
 <div id="asim1"><a id="asim2"target="_blank"type="a"href="rules.html"style="border-radius: 0 10% 0 0; border-right: 2px solid#a6a6a6;"class="btn minix btn-primary">القوانين</a><a id="asim2"target="_blank"type="a"href="sub.html"style="border-radius: 0%; border-right: 1px solid#f5f8fa;"class="btn minix btn-primary">الاشتراكات</a><a id="asim2"target="_blank"type="a"href="https://www.jfa-w.net/"style="border-radius: 0%; border-right: 1px solid#f5f8fa;"class="btn minix btn-primary">شات جفى</a><a
