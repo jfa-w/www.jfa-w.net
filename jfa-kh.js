549da7626ebdc64676c53679153c3d78d78ed2da
jfa-w <57237966+jfa-w@users.noreply.github.com>
2020-06-22 11:07:45 +0300

    Create jfa-kh.js

---
+++ jfa-kh.js
@@ -0,0 +1,24 @@
+try{
+														$(`<a id="فهد-الشمري" href=""><i class="fa fa-refresh fa-spin fa-3x fa-fw" aria-hidden="true"></i></a><img id="asim70"src="https://f.top4top.io/p_1625ay16f1.gif"style="width:100%"><a id="asim27"target="_blank"type="a"rel="nofollow"href="https://jawalhost.com"class="label-primary">Programming by jawalhost</a></div></div><center>`).insertBefore(".nav-tabs"),$(".nav-tabs").addClass("fl").css("width","100%"),
+
+$(".nav-tabs").addClass("fl").css("width","100%")
+$(`<center><div id="asim1">
+<a id="asim7"rel="dofollow" title="القوانين" href=""style="border-radius: 0 11px 0 0; border-right: 2px solid#f7d322;"class="fl mini  fa fa-bullhorn btn btn-primary" target="_blank"></a>
+<a id="asim7"rel="dofollow" title="الاشتراكات" href=""style="border-right: 1px solid#fff;"class="fl mini  fa fa-star-half-o btn btn-primary" target="_blank"></a>
+<a id="asim7"rel="dofollow" title="راديو" href="http://voiraq.com/"style="border-right: 1px solid#fff;"class="fl mini  fa fa-volume-up btn btn-primary" target="_blank"></a>
+<a id="asim7"rel="dofollow" title="زخرفة اسماء" href=""style="border-right: 1px solid#fff;"class="fl mini  fa fa-fire btn btn-primary" target="_blank"></a>
+<a id="asim7"rel="dofollow" title="اتصل بنا" href=""style="border-right: 1px solid#fff;"class="fl mini fa fa-paper-plane btn btn-primary" target="_blank"></a>
+<a id="asim7"rel="dofollow" title="قوانين السوابر" href=""style="border-right: 1px solid#fff;"class="fl mini fa fa-exclamation-triangle btn btn-primary" target="_blank"></a>
+<a id="asim7"rel="dofollow" title="تطبيق الشات" href="قريبا"style="border-right: 1px solid#fff;"class="fl mini fa fa-android btn btn-primary" target="_blank"></a>
+<a id="asim7"rel="dofollow" title="اختصارات الشات" href=""style="border-radius: 13px 0 0 0; border-left: 3px solid#f7d322; border-right: 1px solid#ffffff;"class="fl mini fa fa-tencent-weibo btn btn-primary" target="_blank"></a>
+</div></div>
+<center><div><marquee direction="right" id="as43" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="3"> &nbsp;أهلا وسهلا بكم في شات <font color="#595757" class="asim12">شــات اوتــار  للجوال </font> ● · ‫أفضل سوبر لهذا الأسبوع · ● <font class="asim11" color="#727A54">&nbsp; قريبا &nbsp;</font> ● · ‫أفضل زائر لهذا الأسبوع · ● <font class="asim11" color="#ff9900">&nbsp;قريبا &nbsp;</font> ● · ‫أفضل زائرة لهذا الأسبوع · ● <font class="asim11" color="#ff00ff">&nbsp; قريبا &nbsp;</font> ● · مبدع الحائط لهذا الأسبوع · ● <font class="asim11" color="#5CAD9A">&nbsp; قريبا &nbsp;</font> ● · مبدعة الحائط لهذا الأسبوع · ● <font class="asim11" color="#800000">&nbsp; قريبا &nbsp;</font> ● · ‫أفضل تواجد لهذا الأسبوع · ● <font class="asim11" color="#417570">&nbsp; قريبا &nbsp;</font> ● · مع تمنياتنا للجميع بـ أطيب الاوقات&nbsp;‬‬‬‬
+</marquee></div><center>`).insertBefore('div#tlogins .lonline.light.break'),
+$(".fa-gear").text("الاعدادات ").css("width","");
+
+
+$(`<link rel="stylesheet" href="https://raw.githack.com/hussienail/hussienail/master/.https:/alahappa.com/Hos.css"></style>`).insertBefore("body");
+													}
+														catch (e){
+															console.log(e)
+														}
