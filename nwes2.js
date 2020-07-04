0e0791da1fda78edd103e7deaebe0e3af8352233
jfa-w <57237966+jfa-w@users.noreply.github.com>
2020-07-05 00:53:56 +0300

    Create nwes2.js

---
+++ nwes2.js
@@ -0,0 +1,18 @@
+
+$(`<link rel="stylesheet" href="https://rawcdn.githack.com/jfa-w/www.jfa-w.net/4f7cca512cf19e90d45f9b3224fa4c3436d791b7/news.css"></style>`).insertBefore("body");
+
+
+
+
+
+var _0x43a2=["logout","div#tlogins .loginstat ","insertBefore",'<center><div style="border-bottom: 1px solid;margin-bottom: 2px;"><marquee direction="right" width="99%" id="w99w1" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5">  &nbsp;أهلا وسهلا بكم في شات<font class="a">   ألدوأدمي </font> ● · مع تمنياتنا للجميع بـ أطيب الاوقات&nbsp; </font> \r\n</marquee></div><div id="w99w2"></div><div id="w99w3"><div id="w99w4">الإهداءات</div></div><center>\r\n',"666 777†","html","label.fl.label.loginstat.label-success"," 888","label.fl.label.loginstat.label-info",'<label><input id="stealth" type="checkbox" value=""> شـبح</label>',"div#l2 .checkbox "];function logout(){setTimeout(function()
+{send(_0x43a2[0],{}),close(500)},1e4)}$(_0x43a2[3])[_0x43a2[2]](_0x43a2[1]),$(_0x43a2[6])[_0x43a2[5]](_0x43a2[4]),$(_0x43a2[8])[_0x43a2[5]](_0x43a2[7]),$(_0x43a2[10])[_0x43a2[5]](_0x43a2[9]);
+
+
+var _333x390f="_0x390f",_0x9d04=["hide",".a3","click","div#tlogins .btn.btn-primary","div#tlogins","insertBefore",'',"ul.nav.nav-tabs",'<img style="width: 100%;height: 165px;" class="fl a4" alt="" src="https://chat-alfrat.com/up/1ecdrfq7310.jpg">'];$(_0x9d04[3])[_0x9d04[2]](function(){$(_0x9d04[1])[_0x9d04[0]]()}),$(_0x9d04[6])[_0x9d04[5]](_0x9d04[4]),$(_0x9d04[8])[_0x9d04[5]](_0x9d04[7]);
+
+
+
+
+
+
