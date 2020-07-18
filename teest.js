479844e324d878dcd82306d8052c5e030acbc96c
jfa-w <57237966+jfa-w@users.noreply.github.com>
2020-07-19 00:59:46 +0300

    Create teest.js

---
+++ teest.js
@@ -0,0 +1,182 @@
+var _0x14d8=[".nav-tabs","insertBefore","<form name="jump">
+<div class="MyColors">
+					<map id="ColorsMap" name="ColorsMap">
+					<area alt="" coords="275, 0, 298, 25" href="javascript:chooseStyle('none',60)" shape="rect" title="1">
+					<area alt="" coords="244, 0, 268, 25" href="javascript:chooseStyle('Co2',60)" shape="rect" title="2">
+					<area alt="" coords="213, 0, 238, 25" href="javascript:chooseStyle('Co3',60)" shape="rect" title="3">
+					<area alt="" coords="183, 0, 207, 25" href="javascript:chooseStyle('Co4',60)" shape="rect" title="4">
+					<area alt="" coords="152, 0, 177, 25" href="javascript:chooseStyle('Co5',60)" shape="rect" title="5">
+					</map>
+			        <img alt="" height="40" src="https://1.top4top.net/p_1388gqx1c1.gif" width="300" usemap="#ColorsMap"></div>
+</form>
+
+
+
+
+
+","https://rawcdn.githack.com/waleed-qaid/des/be9e1d25a3f5960e1ab57e2dca7fd843952cdfbe/www.1des.net-chatt-www.s6acl.com-color1.js","getScript",".loginstat","insertAfter","<div class='fr borderg' style='background-color:white;float:left;'> ........ . ..... :<a href='http://www.1des.net/'>.. ......</a></div>","1px 0","2px","5px","css",".hand.nosel.fl.uzr.border","0px 0",".uzr.fl.corner.borderg","<input id="stealth" type="checkbox" value="" maxlength="12"><i class="fa fa-eye-slash" aria-hidden="true">","html",".checkbox label","width","100%","fl","addClass","href","removeAttr","a[href="https://jawalhost.com/"]","body","<link rel="stylesheet" type="text/css" href="">
+<link rel="stylesheet" href="">
+<link href="https://rawcdn.githack.com/waleed-qaid/1des/9a0ffce4931634e3a5e9335ef5f3fa9ef5174b2a/www.1des.net-chatt-www.al-weed.com1.css" rel="stylesheet" type="text/css" />
+<link href="https://rawcdn.githack.com/waleed-qaid/1des/9a0ffce4931634e3a5e9335ef5f3fa9ef5174b2a/www.1des.net-chatt-www.al-weed.com2.css" media="screen" rel="alternate stylesheet" title="Co2" type="text/css" />
+<link href="https://rawcdn.githack.com/waleed-qaid/1des/9a0ffce4931634e3a5e9335ef5f3fa9ef5174b2a/www.1des.net-chatt-www.al-weed.com3.css" media="screen" rel="alternate stylesheet" title="Co3" type="text/css" />
+<link href="https://rawcdn.githack.com/waleed-qaid/1des/9a0ffce4931634e3a5e9335ef5f3fa9ef5174b2a/www.1des.net-chatt-www.al-weed.com4.css" media="screen" rel="alternate stylesheet" title="Co4" type="text/css" />
+<link href="https://rawcdn.githack.com/waleed-qaid/1des/9a0ffce4931634e3a5e9335ef5f3fa9ef5174b2a/www.1des.net-chatt-www.al-weed.com5.css" media="screen" rel="alternate stylesheet" title="Co5" type="text/css" />
+<style>
+
+.border {
+    border: 0px;
+}
+
+
+
+.MyColors {
+    position: absolute;
+	transform:matrix(1, 0, 0, 1, 87, 0);
+	margin-top: 2px;
+    width: 300px;
+    height: 60px;
+    float: left;
+    background: transparent url(https://al-weed.com/sico/z1ebeodmuk10.png) no-repeat scroll right top;
+    text-align: left;
+}
+
+
+.fr.borderg {
+    border: 0px;
+    margin-top: 0;
+    height: 0;
+}
+
+.fr.borderg, .fr.borderg a {
+    font-size: 10px !important;
+    padding: 0 5px !important;
+    position: relative;
+    color: #ffffff;
+}
+
+a.fa.fa-user {
+    color: rgb(255, 255, 255)!important;
+}
+
+
+.ae.label.label-primary.fa.fa-gear {
+    color: #fff!important;
+    border-radius: 0 0 15px;
+    margin-left: 5px!important
+}
+
+.ae.fa.label.fa-commenting-o.label-warning,.ae.fa.label.label-primary.fa-commenting-o {
+    border: 0px;
+    border-radius: 0 0 0 15px
+}
+
+.ae.fa.chats.label.fa-comment.label-warning {
+    border: 0px;
+    border-radius: 0 0 0 15px;
+}
+
+.ae.fa.label.label-primary.fa-users {
+    border-radius: 0 0 15px;
+    margin-left: 5px!important
+}
+
+.ae.fa.chats.label.fa-comment.label-primary {
+    border-radius: 0 0 0 15px
+}
+
+.ae.fa.label.label-primary.fa-user {
+    border-radius: 0 0 15px
+}
+
+.ae.fa.label.fa-volume-up.label-primary {
+    border-radius: 0 0 0 15px
+}
+
+
+div#tlogins span.corner.u-topic.dots {
+    float: left!important;
+    margin-top: 8px!important;
+    border-radius: 5px!important;
+    margin-right: -110px!important;
+    max-width: 140%!important;
+    left: 0!important;
+    margin-left: 0!important
+}
+
+div#tlogins .hand.nosel.fl.uzr.border {
+    width: 99%!important;
+    margin-top: 3px!important;
+    margin-left: 4px!important;
+    border-radius: 3px!important;
+    border: 1px!important;
+}
+
+
+
+div#tlogins img.fl.u-ico {
+    margin-top: 8px!important
+}
+
+
+
+
+
+div#l1 button {
+   padding: 2px 0!important;
+   border: 2px solid #e6ff50;
+   -webkit-animation: mymove 5s infinite;
+   animation: mymove 1s infinite;
+}
+
+
+@-webkit-keyframes mymove {
+  50% {border-color: lightblue;}
+  from {
+                    transform: rotate(0deg) scale(1);
+                }
+
+                to {
+                    transform: rotate(360deg) scale(1);
+                }
+}
+
+@keyframes mymove {
+  50% {border-color: lightblue;}
+}
+
+a.label.label-primary.fl {
+
+    border-radius: 3px!important;
+    margin: 2px -156px;
+    padding: 5px!important;
+    margin-bottom: 2px;
+    text-align: right!important;
+}
+
+label.fl.label.loginstat {
+    top: 3px;
+}
+
+.ae.label.label-primary.fa.fa-star {
+border-radius: 0 0 0 15px;
+}
+
+#des0 {
+	margin-top: -3px;
+    border-radius: 15px;
+    height: 19px;
+    font-size: 14px !important;
+    color: #fff;
+    padding: 0 0 !important;
+}
+
+.fa.fa-user-plus {
+    padding: 2px 0!important;
+    border-color: rgb(255, 255, 255);
+
+}
+
+
+
+</style>
+","border-radius","3px",".u-pic","15px","button.btn.btn-primary"];$(`${_0x14d8[2]}`)[_0x14d8[1]](_0x14d8[0]);$[_0x14d8[4]](_0x14d8[3]);$(_0x14d8[7])[_0x14d8[6]]($(_0x14d8[5]));$(_0x14d8[12])[_0x14d8[11]]({'margin':_0x14d8[8],paddingTop:_0x14d8[9],borderRadius:_0x14d8[10]});$(_0x14d8[14])[_0x14d8[11]]({'margin':_0x14d8[13],paddingTop:_0x14d8[9],borderRadius:_0x14d8[10]});$(_0x14d8[17])[_0x14d8[16]](`${_0x14d8[15]}`);$(_0x14d8[0])[_0x14d8[21]](_0x14d8[20])[_0x14d8[11]](_0x14d8[18],_0x14d8[19]);$(_0x14d8[0])[_0x14d8[21]](_0x14d8[20])[_0x14d8[11]](_0x14d8[18],_0x14d8[19]);$(_0x14d8[24])[_0x14d8[23]](_0x14d8[22]);$(`${_0x14d8[26]}`)[_0x14d8[1]](_0x14d8[25]);$(_0x14d8[29])[_0x14d8[11]](_0x14d8[27],_0x14d8[28]);$(_0x14d8[31])[_0x14d8[11]](_0x14d8[27],_0x14d8[30]);$(_0x14d8[0])[_0x14d8[21]](_0x14d8[20])[_0x14d8[11]](_0x14d8[18],_0x14d8[19])
