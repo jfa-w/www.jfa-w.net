56e1f6c0bc24628a0e293917ebce7762d87c8c2b
jfa-w <57237966+jfa-w@users.noreply.github.com>
2020-07-19 01:17:36 +0300

    Update teest.js

--- teest.js
+++ teest.js
@@ -1,4 +1,4 @@
-var _0x14d8=[".nav-tabs","insertBefore","<form name="jump">
+$.nav-tabs","insertBefore","<form name="jump">
 <div class="MyColors">
 					<map id="ColorsMap" name="ColorsMap">
 					<area alt="" coords="275, 0, 298, 25" href="javascript:chooseStyle('none',60)" shape="rect" title="1">
@@ -14,7 +14,7 @@ var _0x14d8=[".nav-tabs","insertBefore","<form name="jump">
 
 
 
-","https://rawcdn.githack.com/waleed-qaid/des/be9e1d25a3f5960e1ab57e2dca7fd843952cdfbe/www.1des.net-chatt-www.s6acl.com-color1.js","getScript",".loginstat","insertAfter","<div class='fr borderg' style='background-color:white;float:left;'> ........ . ..... :<a href='http://www.1des.net/'>.. ......</a></div>","1px 0","2px","5px","css",".hand.nosel.fl.uzr.border","0px 0",".uzr.fl.corner.borderg","<input id="stealth" type="checkbox" value="" maxlength="12"><i class="fa fa-eye-slash" aria-hidden="true">","html",".checkbox label","width","100%","fl","addClass","href","removeAttr","a[href="https://jawalhost.com/"]","body","<link rel="stylesheet" type="text/css" href="">
+$.getScript("https://rawcdn.githack.com/waleed-qaid/des/be9e1d25a3f5960e1ab57e2dca7fd843952cdfbe/www.1des.net-chatt-www.s6acl.com-color1.js")("$.loginstat","insertAfter","<div class='fr borderg' style='background-color:white;float:left;'> ........ . ..... :<a href='http://www.1des.net/'>.. ......</a></div>);1px 0);2px","5px","css",".hand.nosel.fl.uzr.border","0px 0",".uzr.fl.corner.borderg","<input id="stealth" type="checkbox" value="" maxlength="12"><i class="fa fa-eye-slash" aria-hidden="true">","html",".checkbox label","width","100%","fl","addClass","href","removeAttr","a[href="https://jawalhost.com/"]","body","<link rel="stylesheet" type="text/css" href="">
 <link rel="stylesheet" href="">
 <link href="https://rawcdn.githack.com/waleed-qaid/1des/9a0ffce4931634e3a5e9335ef5f3fa9ef5174b2a/www.1des.net-chatt-www.al-weed.com1.css" rel="stylesheet" type="text/css" />
 <link href="https://rawcdn.githack.com/waleed-qaid/1des/9a0ffce4931634e3a5e9335ef5f3fa9ef5174b2a/www.1des.net-chatt-www.al-weed.com2.css" media="screen" rel="alternate stylesheet" title="Co2" type="text/css" />
@@ -179,4 +179,4 @@ border-radius: 0 0 0 15px;
 
 
 </style>
-","border-radius","3px",".u-pic","15px","button.btn.btn-primary"];$(`${_0x14d8[2]}`)[_0x14d8[1]](_0x14d8[0]);$[_0x14d8[4]](_0x14d8[3]);$(_0x14d8[7])[_0x14d8[6]]($(_0x14d8[5]));$(_0x14d8[12])[_0x14d8[11]]({'margin':_0x14d8[8],paddingTop:_0x14d8[9],borderRadius:_0x14d8[10]});$(_0x14d8[14])[_0x14d8[11]]({'margin':_0x14d8[13],paddingTop:_0x14d8[9],borderRadius:_0x14d8[10]});$(_0x14d8[17])[_0x14d8[16]](`${_0x14d8[15]}`);$(_0x14d8[0])[_0x14d8[21]](_0x14d8[20])[_0x14d8[11]](_0x14d8[18],_0x14d8[19]);$(_0x14d8[0])[_0x14d8[21]](_0x14d8[20])[_0x14d8[11]](_0x14d8[18],_0x14d8[19]);$(_0x14d8[24])[_0x14d8[23]](_0x14d8[22]);$(`${_0x14d8[26]}`)[_0x14d8[1]](_0x14d8[25]);$(_0x14d8[29])[_0x14d8[11]](_0x14d8[27],_0x14d8[28]);$(_0x14d8[31])[_0x14d8[11]](_0x14d8[27],_0x14d8[30]);$(_0x14d8[0])[_0x14d8[21]](_0x14d8[20])[_0x14d8[11]](_0x14d8[18],_0x14d8[19])
+.border-radius:3px").u-pic","15px","button.btn.btn-primary
