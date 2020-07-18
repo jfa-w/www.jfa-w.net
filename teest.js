4832125f9692edec60e3d7b2de832caab8c77dc5
jfa-w <57237966+jfa-w@users.noreply.github.com>
2020-07-19 01:48:26 +0300

    Update teest.js

--- teest.js
+++ teest.js
@@ -1,14 +1,3 @@
-$.nav-tabs").insertBefore("<form name="jump">
-<div class="MyColors">
-					<map id="ColorsMap" name="ColorsMap">
-					<area alt="" coords="275, 0, 298, 25" href="javascript:chooseStyle('none',60)" shape="rect" title="1">
-					<area alt="" coords="244, 0, 268, 25" href="javascript:chooseStyle('Co2',60)" shape="rect" title="2">
-					<area alt="" coords="213, 0, 238, 25" href="javascript:chooseStyle('Co3',60)" shape="rect" title="3">
-					<area alt="" coords="183, 0, 207, 25" href="javascript:chooseStyle('Co4',60)" shape="rect" title="4">
-					<area alt="" coords="152, 0, 177, 25" href="javascript:chooseStyle('Co5',60)" shape="rect" title="5">
-					</map>
-			        <img alt="" height="40" src="https://1.top4top.net/p_1388gqx1c1.gif" width="300" usemap="#ColorsMap"></div>
-</form>
 
 
 
